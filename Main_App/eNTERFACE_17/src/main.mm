2ead8eea91707461488457d85745617fd873ae7e
jorgeMiguelBarreiros <jorge.barreiros.93@gmail.com>
2017-07-12 14:07:23 +0100

    enable orientation = true

--- Main_App/eNTERFACE_17/src/main.mm
+++ Main_App/eNTERFACE_17/src/main.mm
@@ -9,8 +9,8 @@ int main() {
     settings.enableDepth = false; // enables depth buffer for 3d drawing.
     settings.enableAntiAliasing = false; // enables anti-aliasing which smooths out graphics on the screen.
     settings.numOfAntiAliasingSamples = 0; // number of samples used for anti-aliasing.
-    settings.enableHardwareOrientation = false; // enables native view orientation.
-    settings.enableHardwareOrientationAnimation = false; // enables native orientation changes to be animated.
+    settings.enableHardwareOrientation = true; // enables native view orientation.
+    settings.enableHardwareOrientationAnimation = true; // enables native orientation changes to be animated.
     settings.glesVersion = OFXIOS_RENDERER_ES1; // type of renderer to use, ES1, ES2, ES3
     settings.windowMode = OF_FULLSCREEN;
     ofCreateWindow(settings);
